a313d3b47cf18239c7f9a45093144a3cf0b20a80
Md Ismail <mdismaeelkhan345@gmail.com>
2024-07-13 11:25:45 +0530

    updated

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -21,16 +21,11 @@ function Header() {
 
       <div
         onClick={handleLogout}
-        className="cursor-pointer relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-[#3498DB] transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+        className="cursor-pointer relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-[#3498DB] transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-slate-400 group"
       >
         <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#3498DB] group-hover:h-full"></span>
         <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-          <svg
-            className="w-5 h-5 text-green-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+          <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
           >
             <path
               strokeLinecap="round"
@@ -56,7 +51,7 @@ function Header() {
             ></path>
           </svg>
         </span>
-        <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+        <span className="relative w-full text-white text-left transition-colors duration-200 ease-in-out group-hover:text-white">
           Logout
         </span>
       </div>
